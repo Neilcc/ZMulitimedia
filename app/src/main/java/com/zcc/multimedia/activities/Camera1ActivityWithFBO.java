048c940dbfb839358c4622809ce6ece7493914d4
zhuchengcheng <zhuchengcheng@netease.corp.com>
2018-07-25 19:10:54 +0800

    add fbo

--- app/src/main/java/com/zcc/multimedia/activities/Camera1ActivityWithFBO.java
+++ app/src/main/java/com/zcc/multimedia/activities/Camera1ActivityWithFBO.java
@@ -1,5 +1,7 @@
 package com.zcc.multimedia.activities;
 
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,12 +12,17 @@ import android.view.View;
 import com.zcc.multimedia.R;
 import com.zcc.multimedia.egl.EGLHandlerThread;
 import com.zcc.multimedia.gles.GLESHelper;
+import com.zcc.multimedia.grafika.gles.GlUtil;
 
 public class Camera1ActivityWithFBO extends AppCompatActivity implements SurfaceHolder.Callback,
         View.OnClickListener {
 
     private SurfaceView mSurfaceView;
     private EGLHandlerThread mSurfaceThread;
+
+    private int[] glTexture = new int[1];
+    private int[] glFBO = new int[1];
+    private int[] glRenderBuffer = new int[1];
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -54,6 +61,22 @@ public class Camera1ActivityWithFBO extends AppCompatActivity implements Surface
     }
 
     private void createFBO() {
+        GLES20.glGenTextures(1,glTexture, 0);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, glTexture[0]);
+        GlUtil.checkGlError("glBindTexture ");
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER,
+                GLES20.GL_NEAREST);
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER,
+                GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S,
+                GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T,
+                GLES20.GL_CLAMP_TO_EDGE);
+        GlUtil.checkGlError("glTexParameter");
+
+        GLES20.glGenRenderbuffers(1, glRenderBuffer , 0);
+        GLES20.glGenFramebuffers(1, glFBO, 0);
 
     }
 
